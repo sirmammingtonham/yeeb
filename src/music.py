5ec3aca11ebb178e48361868b89e9e1047876ba8
William Chang <wfchang@usc.edu>
2020-07-29 04:55:45 -0700

    bruh that

--- src/music.py
+++ src/music.py
@@ -523,6 +523,18 @@ class Music(commands.Cog):
         #     await ctx.send(f'This is so sad, Alexa play {song_split[0]} by {song_split[-1]}')
         #     await self.play_.callback(self, ctx, search=song)
 
+    @commands.command(name='that')
+    async def that_(self, ctx, *args):
+        mediawikiapi = MediaWikiAPI()
+        articles = []
+        
+        for _ in range(2):
+            article = mediawikiapi.random()
+            articles.append('[{}]({})'.format(
+                article, 'https://en.wikipedia.org/wiki/'+article.replace(' ', '_')))
+
+        await ctx.send('That is so {1}, can we hit {0} {2}'.format(random.randint(0, 10000000), *articles))
+
     @commands.command(name='finna', aliases=['smash', 'finna_smash', 'finna-smash'])
     async def finna_(self, ctx, *args):
         await self.play_.callback(self, ctx, search=random.choice(smash))
