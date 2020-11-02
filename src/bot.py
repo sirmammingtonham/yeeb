71b6fd190d1d505a99484c62551bec58a87a01bf
Shemida <60976014+Shemida@users.noreply.github.com>
2020-11-02 03:24:54 -0800

    Intents for 1.5.0

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,10 @@ from discord.ext import commands
 
 with open("../res/token.txt", "r") as f:
     TOKEN = f.read()
-bot = commands.Bot(command_prefix = 'bruh ')
+# Configure intents (1.5.0)
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix = 'bruh', guild_subscriptions=True, intents=intents) 
 bot.remove_command('help')
 
 extensions = ['bruh', 'music', 'events', 'card', 'speech']
