9ca16173b0d6a9610dd19ed31d6f84d1cdd92e05
William Chang <wfchang@usc.edu>
2020-11-02 03:55:13 -0800

    add intents back

--- src/bot.py
+++ src/bot.py
@@ -3,10 +3,14 @@ sys.path.append("../discord.py/")
 import discord
 from discord.ext import commands
 
+print('discord version', discord.__version__)
 
 with open("../res/token.txt", "r") as f:
     TOKEN = f.read()
-bot = commands.Bot(command_prefix = 'bruh', guild_subscriptions=True) 
+# Configure intents (1.5.0)
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix = 'bruh', guild_subscriptions=True, intents=intents) 
 bot.remove_command('help')
 
 extensions = ['bruh', 'music', 'events', 'card', 'speech']
